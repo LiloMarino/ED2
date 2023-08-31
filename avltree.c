a71a157359d5c9687c3ea42926ac5fca3a341a37
LiloMarino <lilo.marino@hotmail.com>
2023-08-30 22:36:27 -0300

    Implementado FreeAVL function to recursively free all nodes in the AVL tree, rather than just the root node.

--- avltree.c
+++ avltree.c
@@ -130,6 +130,52 @@ void RemoveNodeAVL(DataStructure AVLTree, Node N)
 {
 }
 
-void FreeAVL(DataStructure AVLTree)
+void FreeAVL(DataStructure *AVLTree)
 {
+    Raiz *Tree = *AVLTree;
+    NodeTree *No = Tree->No;
+    NodeTree *Clear = NULL;
+    while (Tree->NumTotalNos != 0)
+    {
+        bool Vazio = true;
+        if (No->Dir != NULL)
+        {
+            /*Primeiro filho diferente de NULL encontrado do nó*/
+            No = No->Dir;
+            Vazio = false;
+        }
+        else if (No->Esq != NULL)
+        {
+            /*Primeiro filho diferente de NULL encontrado do nó*/
+            No = No->Esq;
+            Vazio = false;
+        }
+        if (Vazio)
+        {
+            /*Nó não tem filhos*/
+            Clear = No;
+            No = No->Pai;
+            free(Clear);
+            if (No != NULL)
+            {
+                /*Atribui NULL ao filho desalocado*/
+                if (No->Dir != NULL)
+                {
+                    No->Dir = NULL;
+                }
+                else if (No->Esq != NULL)
+                {
+                    No->Esq = NULL;
+                }
+            }
+            else
+            {
+                /*Nó raiz*/
+                Tree->node = NULL;
+                free(Tree);
+                *AVLTree = NULL;
+                return;
+            }
+        }
+    }
 }
