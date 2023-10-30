fac749ae95c5589d19b3cfaca291aaa417ab7431
LiloMarino <lilo.marino@hotmail.com>
2023-10-30 16:09:33 -0300

    Reimplementado Dijkstra utilizando o heap

--- grafos.h
+++ grafos.h
@@ -16,7 +16,7 @@ DataStructure criarGrafo(bool direcionado);
 /**
  * @brief Verifica se existe o vértice no grafo
  * @param grafo Ponteiro para o grafo
- * @param vertice Número do vertice 
+ * @param vertice Número do vertice
  * @return Se existir o vértice no grafo ele retorna verdadeiro, caso contrário retorna falso
  */
 bool existsVerticeIn(DataStructure grafo, int vertice);
@@ -24,7 +24,7 @@ bool existsVerticeIn(DataStructure grafo, int vertice);
 /**
  * @brief Procura o vértice
  * @param grafo Ponteiro para o grafo
- * @param vertice Número do vertice 
+ * @param vertice Número do vertice
  * @return Retorna o vértice
  */
 Node searchVertice(DataStructure grafo, int vertice);
@@ -32,7 +32,7 @@ Node searchVertice(DataStructure grafo, int vertice);
 /**
  * @brief Procura o vértice
  * @param grafo Ponteiro para o grafo
- * @param vertice Número do vertice 
+ * @param vertice Número do vertice
  * @return Retorna o índice do vértice
  */
 int searchVerticeID(DataStructure grafo, int vertice);
@@ -40,8 +40,8 @@ int searchVerticeID(DataStructure grafo, int vertice);
 /**
  * @brief Adiciona um vértice ao grafo
  * @param grafo Ponteiro para o grafo
- * @param src Número do vértice de origem 
- * @param dest Número do vértice de destino 
+ * @param src Número do vértice de origem
+ * @param dest Número do vértice de destino
  * @param peso Peso do caminho entre o vértice de origem e o vértice de destino
  * @note Se o peso for 0 então é considerado que não existe peso entre os nós do grafo
  */
@@ -72,33 +72,6 @@ void printGrafo(DataStructure grafo);
  * @param inicio Número do vértice de início
  */
 void executarDijkstra(DataStructure grafo, int inicio);
-
-/**
- * @brief Verifica se há vértices abertos no grafo
- * @param grafo Ponteiro para o grafo
- * @param aberto Vetor indicando se os vértices estão abertos
- * @return true se há vértices abertos, false caso contrário
- */
-bool existeAberto(DataStructure grafo, bool aberto[]);
-
-/**
- * @brief Encontra o índice do vértice com a menor distância entre os vértices abertos
- * @param grafo Ponteiro para o grafo
- * @param aberto Vetor indicando se os vértices estão abertos
- * @param dist Vetor de distâncias
- * @return Índice do vértice com a menor distância
- */
-int menorDist(DataStructure grafo, bool aberto[], int dist[]);
-
-/**
- * @brief Relaxa as arestas do vértice atual, atualizando distâncias e predecessores se necessário
- * @param grafo Ponteiro para o grafo
- * @param index Índice do vértice atual
- * @param dist Vetor de distâncias
- * @param pred Vetor de predecessores
- * @param vizinhoIndex Índice do vértice vizinho
- */
-void relaxaAresta(DataStructure grafo, int index, int dist[], int pred[], int vizinhoIndex);
 
 /**
  * @brief Imprime as distâncias mínimas na tela
--- heap.h
+++ heap.h
@@ -2,6 +2,7 @@
 #define HEAP_H
 
 #include <stddef.h>
+#include <stdbool.h>
 
 typedef void *Item;
 typedef void *Heap;
@@ -15,7 +16,7 @@ Heap criarHeap(size_t capac);
 
 /**
  * @brief Adiciona um novo elemento à heap
- * @param H Heap onde a operação será realizada
+ * @param H Ponteiro para o Heap
  * @param item Elemento a ser adicionado à heap
  * @param prioridade Prioridade associada ao elemento
  */
@@ -23,10 +24,17 @@ void heapPush(Heap H, Item item, int prioridade);
 
 /**
  * @brief Remove e retorna o elemento de maior prioridade da heap
- * @param H Heap onde a operação será realizada
+ * @param H Ponteiro para o Heap
  * @return Elemento de maior prioridade removido da heap
  */
 Item heapPop(Heap H);
+
+/**
+ * @brief Verifica se o Heap está vazio
+ * @param H Ponteiro para o Heap
+ * @return Retorna verdadeiro caso esteja vazio e falso caso contrário
+ */
+bool isEmptyHeap(Heap H);
 
 /**
  * @brief Libera a memória alocada para a heap
@@ -36,18 +44,16 @@ void killHeap(Heap H);
 
 /**
  * @brief Restaura a propriedade de heap da árvore a partir de um determinado índice
- * @param H Heap onde a operação será realizada
+ * @param H Ponteiro para o Heap
  * @param current_index Índice a partir do qual a propriedade de heap será restaurada
  */
 void heapifyDown(Heap H, size_t current_index);
 
 /**
  * @brief Restaura a propriedade de heap subindo na árvore a partir de um determinado índice
- * @param H Heap onde a operação será realizada
+ * @param H Ponteiro para o Heap
  * @param current_index Índice a partir do qual a propriedade de heap será restaurada
  */
 void heapifyUp(Heap H, size_t current_index);
-
-
 
 #endif
